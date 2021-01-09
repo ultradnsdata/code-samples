1b97dd56710c9d802e3c772d59754eccaca6ae67
Bill Snow <bill.snow@team.neustar>
2021-01-09 01:58:19 -0500

    use a credentials file

--- tools/ultradatagettoken.py
+++ tools/ultradatagettoken.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 import requests
+from pathlib import Path
 
 def ultradata_login():
 
-    #todo: use ~/.ultradata/credentials
-    #  consider .netrc format, which 'requests' understands - but might be annoying
-    #  https://docs.python.org/3/library/netrc.html
-    #with open('.replace('\n', '')', 'r') as file:
-    #    data = file.readlines()
-    
-    ULTRA_USERNAME='ultradata-miranda-full'
-    ULTRA_PASSWORD='ThePasswordIs1Password'
-    URL='https://api.miranda.neustar/v1'
+    with open(str(Path.home())+"/.ultradata/credentials", 'r') as file:
+        data = file.readlines()
+        for line in data:
+            if line.startswith("ULTRA_USERNAME="):
+                ULTRADATA_USERNAME=line[15:].strip()
+            if line.startswith("ULTRA_PASSWORD="):
+                ULTRADATA_PASSWORD=line[15:].strip()
+
+    URL='https://api.ultradata.neustar/v1'
    
     auth_response = requests.post(URL+'/authorization/token',
                                  data = {
                                      'grant_type': 'password',
-                                     'username': ULTRA_USERNAME,
-                                     'password': ULTRA_PASSWORD
+                                     'username': ULTRADATA_USERNAME,
+                                     'password': ULTRADATA_PASSWORD
                                  })
-    #throw something if there is any problem with the authentication
     auth = auth_response.json()
     return auth['access_token']
+
 
 if __name__ == '__main__':
     print(ultradata_login())
