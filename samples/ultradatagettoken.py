0bedd8eb0c517c59c7655afa536d8ee5c4befc94
Bill Snow <bill.snow@team.neustar>
2021-01-13 16:56:35 -0500

    satisfy pep8

--- samples/ultradatagettoken.py
+++ samples/ultradatagettoken.py
@@ -2,24 +2,25 @@
 import requests
 from pathlib import Path
 
+
 def ultradata_login():
 
     with open(str(Path.home())+"/.ultradata/credentials", 'r') as file:
         data = file.readlines()
         for line in data:
             if line.startswith("ULTRA_USERNAME="):
-                ULTRADATA_USERNAME=line[15:].strip()
+                ULTRADATA_USERNAME = line[15:].strip()
             if line.startswith("ULTRA_PASSWORD="):
-                ULTRADATA_PASSWORD=line[15:].strip()
+                ULTRADATA_PASSWORD = line[15:].strip()
 
-    URL='https://api.ultradata.neustar/v1'
-   
+    URL = 'https://api.ultradata.neustar/v1'
+
     auth_response = requests.post(URL+'/authorization/token',
-                                 data = {
-                                     'grant_type': 'password',
-                                     'username': ULTRADATA_USERNAME,
-                                     'password': ULTRADATA_PASSWORD
-                                 })
+                                  data={
+                                      'grant_type': 'password',
+                                      'username': ULTRADATA_USERNAME,
+                                      'password': ULTRADATA_PASSWORD
+                                  })
 
     auth = auth_response.json()
     try:
--- samples/ultradataqueries.py
+++ samples/ultradataqueries.py
@@ -8,62 +8,65 @@ from dateutil.relativedelta import *
 
 import ultradatagettoken
 
-def ultradata_api(token,function,params):
+
+def ultradata_api(token, function, params):
     response_code = requests.codes.accepted
     while response_code == requests.codes.accepted:
-        print('Requesting %s'%(function))
+        print('Requesting %s' % (function))
         activity_response = requests.get(URL+function,
-                                         #allow_redirects=False,
+                                         # allow_redirects=False,
                                          params=params,
                                          headers={
                                              'Authorization': 'Bearer '+token
                                          })
         response_code = activity_response.status_code
         if response_code == requests.codes.accepted:
-            print('Received a 202 for domain/activity.  Have to wait and try again')
+            print('Received a 202 for domain/activity.  '
+                  + 'Have to wait and try again')
             sleep(activity_response.json()['poll-interval-s'])
-
 
     if activity_response.status_code == requests.codes.ok:
         print(activity_response.json())
     else:
-        print('Response code: %s'%(activity_response.status_code))
+        print('Response code: %s' % (activity_response.status_code))
         print()
         print(activity_response.json())
 
+
 if __name__ == '__main__':
-    URL='https://api.ultradata.neustar/v1'
-    NOW=datetime.now()
-    STARTTIME=int((NOW+relativedelta(days=-6)).timestamp())
-    ENDTIME=int((NOW+relativedelta(days=-2)).timestamp())
+    URL = 'https://api.ultradata.neustar/v1'
+    NOW = datetime.now()
+    STARTTIME = int((NOW+relativedelta(days=-6)).timestamp())
+    ENDTIME = int((NOW+relativedelta(days=-2)).timestamp())
     try:
         token = ultradatagettoken.ultradata_login()
     except Exception as e:
         print('Login failure')
         sys.exit(1)
 
+    # note that /domain/zonedomains will return an empty set: neustar.biz is
+    # not a ns name like pdns196.ultradns.biz
     for function in [
             "/domain/activity",
             "/domain/hostips",
             "/domain/subdomains",
-            "/domain/zonedomains", #will return an empty set: neustar.biz is not a ns name like pdns196.ultradns.biz
+            "/domain/zonedomains",
             "/domain/domainnameservers",
             "/domain/extra"]:
-        
-        ultradata_api(token,function,{
+
+        ultradata_api(token, function, {
             'domain': 'neustar.biz',
             'starttime': STARTTIME,
             'endtime': ENDTIME
         })
     print()
-    ultradata_api(token,"/address/hostdomainnames",{
+    ultradata_api(token, "/address/hostdomainnames", {
         'cidr': '156.154.240.232-32',
         'starttime': STARTTIME,
         'endtime': ENDTIME
     })
-    ultradata_api(token,"/address/netactivity",{
+    ultradata_api(token, "/address/netactivity", {
         'cidr': '156.154.240.232-32',
         'starttime': STARTTIME,
         'endtime': ENDTIME
     })
-
